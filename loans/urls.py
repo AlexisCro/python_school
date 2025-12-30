b86972dd65d916552ecc20eec4685a71c50028bc
alexis-pa <alexis.crozier@spendhq.com>
2025-12-30 17:00:47 +0100

    start return

--- loans/urls.py
+++ loans/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
   path('', views.index_view, name='loans'),
   path('<int:loan_id>', views.show_views, name='loan'),
   path('new/', views.new_view, name='new_loan'),
-  path('new/loans/create/', views.create_view, name='create_loan')
+  path('new/loans/create/', views.create_view, name='create_loan'),
+  path('return/<int:loan_id>', views.return_views, name='return')
 ]
